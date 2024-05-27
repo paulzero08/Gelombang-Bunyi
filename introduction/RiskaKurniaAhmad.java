b3a31a3e99f1f242c3ebe9800406c6240bc0910f
paulzero08 <92966424+paulzero08@users.noreply.github.com>
2024-05-27 09:32:34 +0700

    Add files via upload

---
+++ introduction/RiskaKurniaAhmad.java
@@ -0,0 +1,17 @@
+public class RiskaKurniaAhmad {
+    public static void main(String[] args) {
+        // Data Diri
+        String fullname = "Riska kurnia ahmad";
+        String nim  = "2213010346";
+        String team = "Java Limited Edition";
+        String role = "Programmer"
+ 
+
+        // Menampilkan Data Diri
+        System.out.println("Perkenalkan nama saya " + fullname +"." );
+        System.out.println("Nim saya =  " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+
+    }
+}
