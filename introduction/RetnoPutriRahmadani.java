071a4dc144a6018e148c44113e061b62ec33525b
retnoputr <169017613+retnoputr@users.noreply.github.com>
2024-06-23 13:55:43 +0700

    Add files via upload

---
+++ introduction/RetnoPutriRahmadani.java
@@ -0,0 +1,17 @@
+public class RetnoPutriRahmadani {
+    public static void main(String[] args) {
+        // Data Diri
+        String fullname = "Retno Putri Rahmadani";
+        String nim  = "2213010358";
+        String team = "Java Limited Edition";
+        String role = "Designer"
+ 
+
+        // Menampilkan Data Diri
+        System.out.println("Perkenalkan nama saya " + fullname +"." );
+        System.out.println("Nim saya =  " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+
+    }
+}
